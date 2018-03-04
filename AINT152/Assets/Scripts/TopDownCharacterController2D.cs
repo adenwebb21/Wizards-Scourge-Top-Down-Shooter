da1c4c90cbc367191f5e6247b4b7ed4ef602f456
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-03-04 13:49:31 +0000

    most up to date version

---
+++ AINT152/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+    Animator anim;
+
+    public GameObject shot;
+    public float fireRate;
+
+    private float nextFire;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, transform.position, transform.rotation);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if(x == 0 && y == 0)
+        {
+            anim.SetInteger("state", 0);
+        }
+        else
+        {
+            anim.SetInteger("state", 1);
+        }
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+
+
+}

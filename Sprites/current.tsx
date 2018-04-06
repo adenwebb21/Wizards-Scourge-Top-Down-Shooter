660071ec6a4a3538b30380a92e3eb6251faa2f6d
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-04-06 14:11:20 +0100

    organised all files from tablet and desktop together

--- Sprites/current.tsx
+++
@@ -1,5 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="current" tilewidth="64" tileheight="64" tilecount="49" columns="7">
- <image source="spritesheet.png" trans="ff60e4" width="472" height="472"/>
- <tile id="0" probability="0.5"/>
-</tileset>

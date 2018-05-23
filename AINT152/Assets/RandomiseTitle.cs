5f533d2b69320b8f770b9458d05fdd5408f4c2e3
Aden Webb <33032049+adenwebb21@users.noreply.github.com>
2018-05-23 17:17:57 +0100

    added goblin party 1 in 100

---
+++ AINT152/Assets/RandomiseTitle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomiseTitle : MonoBehaviour
+{
+    private Text title;
+
+    void Start()
+    {
+        title = gameObject.GetComponent<Text>();
+
+        if(Random.Range(0, 100) == 100)
+        {
+            title.text = "~ Goblin Party!! ~";
+        }
+    }
+
+}
